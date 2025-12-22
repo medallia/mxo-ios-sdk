6a60c6e91a3a16813735aad0916c676f404fd968
release-mobile <decibel-release-mobile@medallia.com>
2025-12-22 12:17:26 -0500

    Jenkins Release Job 30 message: chore(release): Release Update

--- Package.swift
+++ Package.swift
@@ -1,24 +1,46 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 //  
-//  Copyright © 2023 Medallia. Use subject to licensing terms.
+//  Copyright © 2025 Medallia. Use subject to licensing terms.
 
+import PackageDescription
+
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
     name: "medallia-mxo-ios-sdk",
     platforms: [
-            .iOS(.v13)
-        ],
+        .iOS(.v13)
+    ],
     products: [
         .library(
             name: "medallia-mxo-ios-sdk",
-            targets: ["MedalliaMXO"]),
+            targets: ["MedalliaMXOWrapper"]
+        )
+    ],
+    dependencies: [
+        .package(
+            name: "medallia-mobile-bridge-ios-sdk",
+            url: "https://github.com/medallia/mobile-ios-bridge-sdk.git",
+            .upToNextMajor(from: "1.0.0")
+        )
     ],
     targets: [
         .binaryTarget(
             name: "MedalliaMXO",
             path: "MedalliaMXO.embeddedframework/MedalliaMXO.xcframework"
+        ),
+        .target(
+            name: "MedalliaMXOWrapper",
+            dependencies: [
+                .target(name: "MedalliaMXO"),
+                .product(
+                    name: "medallia-mobile-bridge-ios-sdk",
+                    package: "medallia-mobile-bridge-ios-sdk"
+                )
+            ],
+            path: "MedalliaMXOWrapper"
         )
     ]
 )
